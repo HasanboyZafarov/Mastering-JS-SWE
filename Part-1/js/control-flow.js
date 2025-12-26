aa0b38869142524afbb74832f5e1ef00bcc5d9f9
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-26 20:02:31 +0500

    Do ... While

--- Part-1/js/control-flow.js
+++ Part-1/js/control-flow.js
@@ -49,8 +49,18 @@
 // }
 
 // While
-let i = 0;
-while (i <= 5) {
-  if (i % 2 != 0) console.log(i);
-  i++;
-}
+// let i = 9;
+// while (i <= 5) {
+//   if (i % 2 != 0) console.log(i);
+//   i++;
+// }
+
+// Do ... While
+// let i = 9;
+// do {
+//   if (i % 2 != 0) console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// Do ... While will run at least once even if the condition is false
+
