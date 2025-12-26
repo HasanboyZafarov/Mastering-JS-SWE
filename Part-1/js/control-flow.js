25609468222aface1ec0af5b1bc5fc0b3e81d958
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-26 20:07:38 +0500

    Infinite loops

--- Part-1/js/control-flow.js
+++ Part-1/js/control-flow.js
@@ -64,3 +64,16 @@
 
 // Do ... While will run at least once even if the condition is false
 
+// Infinite loops
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// while (true) {}
+// do {} while (true);
+// for (let i = 0; i > 0; i++) {}
+
+// Avoid infinite loops by ensuring the loop condition will eventually be false.
