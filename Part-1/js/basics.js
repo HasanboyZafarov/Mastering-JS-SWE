a9dd11ab269c009e52d1b6cad90ca8605f934132
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-26 15:45:22 +0500

    Variables

---
+++ Part-1/js/basics.js
@@ -0,0 +1,14 @@
+// Variables
+let name = "David";
+console.log(name);
+
+// Cannot be a reserved keywords
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// They are case-sensitive
+
+let firstName = "Mosh";
+let lastName = "Hamedani";
+// camel Notation
+// camelCase => first word should be lower case, and first letter of every after word must be UpperCase
