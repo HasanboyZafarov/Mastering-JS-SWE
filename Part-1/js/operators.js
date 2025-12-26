fae3457aa2f835f3fa1f0e286dddc88206cac72d
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-26 17:31:08 +0500

    Arythmetic Operators

---
+++ Part-1/js/operators.js
@@ -0,0 +1,24 @@
+// Operators in JavaScript
+
+// Arithmetic, Assignment, Comparison, Logical, and Bitwise Operators
+
+// Operators variables -> Expressions -> Algorithms -> Programs
+
+// Arythmetic Operators
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x + y); // Addition
+// console.log(x - y); // Subtraction
+// console.log(x * y); // Multiplication
+// console.log(x / y); // Division
+// console.log(x % y); // Modulus | Remainder
+// console.log(x ** y); // Exponentiation | 10^3 = 10*10*10 = 1000
+
+// Increment
+// console.log(x); // 10
+// console.log(x++); // 11
+
+// Decrement Operators
+// console.log(--x); // 9
