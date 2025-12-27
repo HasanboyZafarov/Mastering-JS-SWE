f8acaa9a401d32d22044d68ed27c2e4a2d28d89e
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-27 15:13:02 +0500

    Basics of Object

---
+++ Part-1/js/objects.js
@@ -0,0 +1,19 @@
+// let radius = 1;
+// let x = 1;
+// let y = 1;
+
+// Object-Oriented Programming (OOP)
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("Draw");
+  },
+};
+
+circle.draw(); // We call it method
