a26acbb1f5940c13b8893e6dd2611d209b1383ea
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-27 14:16:55 +0500

    Done with Control Flow

--- Part-1/js/control-flow.js
+++ Part-1/js/control-flow.js
@@ -102,14 +102,156 @@
 
 // Break and Continue`
 
-let i = 0;
-while (i <= 10) {
-  // if (i === 5) break;
-  if (i % 2 == 0) {
-    i++;
-    continue;
-  }
-  
-  console.log(i);
-  i++;
-}
+// let i = 0;
+// while (i <= 10) {
+// if (i === 5) break;
+//   if (i % 2 == 0) {
+//     i++;
+//     continue;
+//   }
+
+//   console.log(i);
+//   i++;
+// }
+
+// Task 1
+
+// Write a function that takes two numbers
+// and returns the maximum of the two
+
+// function max(x, y) {
+// if (x > y) return x;
+// else return y;
+// return x > y ? x : y;
+// }
+// console.log(max(5, 3)); // 5
+
+// Landscape or Portrait
+// function checkImg(width, height) {
+//   return width > height ? "Landscape" : "Portrait";
+// }
+// console.log(checkImg(200, 500));
+
+// FizzBuzz
+// function fizzBuzz(num) {
+//   if (num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+//   else if (num % 3 == 0) return "Fizz";
+//   else if (num % 5 == 0) return "Buzz";
+//   else return "Not FizzBuzz";
+// }
+
+// console.log(fizzBuzz(10));
+
+// SpeedLimit
+// Limit = 70
+// 5 -> 1 point
+// 12 points -> suspended
+
+// function checkSpeed(speed) {
+//   const speedLimit = 70;
+//   const kmPerPoint = 5;
+//   const points = Math.floor(Math.max(0, speed - speedLimit) / kmPerPoint);
+//   if (points >= 12) {
+//     console.log("License suspended");
+//   } else if (points > 0) {
+//     console.log("Points", points);
+//   } else {
+//     console.log("Ok");
+//   }
+// }
+// checkSpeed(74);
+
+// Even or Odd Numbers
+
+// function showNumbers(limit) {
+//   for (let i = 0; i <= limit; i++) {
+//     console.log(i % 2 == 0 ? `${i} EVEN` : `${i} ODD`);
+//   }
+// }
+// showNumbers(10);
+
+// Count Truthy
+
+// function countTruthy(array) {
+//   let counter = 0;
+//   for (key of array) {
+//     if (key) counter++;
+//   }
+//   console.log(counter);
+// }
+// countTruthy([1, "", false, true, "hello", undefined]);
+
+// let movie = {
+//   title: "a",
+//   releaseYear: 2018,
+//   rating: 4.5,
+//   director: "b",
+// };
+
+// function showProperties(obj) {
+//   for (key in obj) if (typeof obj[key] == "string") console.log(key, obj[key]);
+// }
+// showProperties(movie);
+
+// function sum(limit) {
+//   let counter = 0;
+//   for (let i = 0; i <= 10; i++) {
+//     if (i % 3 == 0 || i % 5 == 0) counter += i;
+//   }
+//   return counter;
+// }
+// console.log(sum(10));
+
+// let marks = [80, 80, 50];
+
+// Avarage = 70
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+// function calculateGrade(marks) {
+//   let counter = 0;
+//   for (key of marks) counter += key;
+//   let avarage = counter / marks.length;
+//   console.log(avarage);
+//   if (avarage >= 90 && avarage <= 100) return "A";
+//   else if (avarage >= 80 && avarage <= 89) return "B";
+//   else if (avarage >= 70 && avarage <= 79) return "C";
+//   else if (avarage >= 60 && avarage <= 69) return "D";
+//   if (avarage >= 1 && avarage <= 59) return "F";
+// }
+// console.log(calculateGrade(marks));
+
+// function showStars(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let stars = "";
+//     for (let k = 0; k < i; k++) {
+//       stars += "*";
+//     }
+//     console.log(stars);
+//   }
+// }
+// showStars(4);
+
+// function showPrimes(limit) {
+//   for (let i = 2; i <= limit; i++) {
+//     let isPrime = true;
+
+//     for (let j = 2; j <= Math.sqrt(i); j++) {
+//       if (i % j === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+
+//     if (isPrime) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// showPrimes(10);
+
+// End of the Control Flow
