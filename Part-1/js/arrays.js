2c3bd9fd4d76896fd3b0bee29fe8ae8d6e527ded
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-28 23:34:50 +0500

    Removing Elements

--- Part-1/js/arrays.js
+++ Part-1/js/arrays.js
@@ -45,3 +45,22 @@
 
 // console.log(courses.includes({ id: 1, name: "a" }));
 // let found = courses.findIndex((course) => course.name === "b");
+
+// Removing Element in Array;
+
+// let numbers = [1, 2, 3, 4];
+
+// End
+// let last = numbers.pop();
+// console.log(numbers);
+// console.log(last);
+
+// Beginning
+// let start = numbers.shift();
+// console.log(numbers);
+// console.log(start);
+
+// Middle
+// let middle = numbers.splice(2, 2);
+// console.log(numbers);
+// console.log(middle);
