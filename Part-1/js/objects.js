f5f563e0d0aca481a66abcadcef6db7d0bf05eae
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-28 18:12:21 +0500

    Date

--- Part-1/js/objects.js
+++ Part-1/js/objects.js
@@ -210,3 +210,12 @@
 // Mosh`;
 
 // console.log(another);
+
+// Date
+
+// const now = new Date();
+// const date1 = new Date("May 11 2018 09:00");
+// const date2 = new Date(2018, 4, 11, 9);
+
+// now.setFullYear(2017);
+// console.log(now);
