e1d454c999f7c762712460c3954c8a622b372a44
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-29 15:42:25 +0500

    Getters and Setters

--- Part-1/js/functions.js
+++ Part-1/js/functions.js
@@ -35,3 +35,26 @@
 // }
 
 // console.log(interest(10000, undefined, 5));
+
+// Getters and Setters
+const person = {
+  firstName: "Mosh",
+  lastName: "Hamedani",
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value) {
+    const parts = value.split(" ");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+person.fullName = "John Smith";
+
+// getters => access properties
+// setters => change (mutate) them
+
+// console.log(person.fullName);
+
+// console.log(person);
