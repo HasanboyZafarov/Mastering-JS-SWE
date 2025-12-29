f72dcd43a7ac743edb7277cf42d7b6aa395f9943
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-29 14:52:54 +0500

    Arguments

--- Part-1/js/functions.js
+++ Part-1/js/functions.js
@@ -10,3 +10,14 @@
 // let move = run;
 // run();
 // move();
+
+// Arguments
+
+// function sum() {
+//   let total = 0;
+//   for (value of arguments) {
+//     total += value;
+//   }
+//   return total;
+// }
+// console.log(sum(1, 2, 3, 4, 5, 10));
