64d50571fd29e0fdcd58698b1c9b62dd8838b774
Hasanboy Zafarov <dewozto1337@gmail.com>
2025-12-29 16:20:25 +0500

    Let vs Var

--- Part-1/js/functions.js
+++ Part-1/js/functions.js
@@ -103,3 +103,27 @@
 // }
 
 // start();
+
+// Let vs Var
+
+// function start() {
+//   for (var i = 0; i < 5; i++) {
+//     console.log(i);
+//     if (true) {
+//       var color = "red";
+//     }
+//   }
+//   console.log(color);
+// }
+
+// var => function-scoped
+// ES6 (ES2015):let, const => block-scoped
+
+// start();
+
+// var color = "red";
+// let age = 30;
+
+// function sayHi() {
+//   console.log("Hi");
+// }
