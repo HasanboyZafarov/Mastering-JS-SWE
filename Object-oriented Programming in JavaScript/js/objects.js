fe8f2777f59c95d366087f58db47b76cd6275749
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-02 21:29:58 +0500

    Done with Objects (only theory

--- Object-oriented Programming in JavaScript/js/objects.js
+++ Object-oriented Programming in JavaScript/js/objects.js
@@ -41,3 +41,88 @@
 
 // const another = new Circle(1);
 // another.draw();
+
+// Functions are Objects
+
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("another - draw");
+//   };
+// }
+
+// Circle.call({}, 1);
+
+// const another = new Circle(1);
+
+// Value and Reference Types
+
+// let obj = { value: 10 };
+
+// function increse(obj) {
+//   obj.value++;
+// }
+// increse(obj);
+// console.log(obj);
+
+// Adding and Removing Properties
+
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("draw");
+//   };
+// }
+
+// const circle = new Circle(10);
+
+// circle.location = { x: 1, y: 2 };
+// circle["isLogged"] = true;
+
+// Enumirating Properties
+
+// for (let key in circle) {
+//   if (typeof circle[key] !== "function") console.log(key, circle[key]);
+// }
+
+// const keys = Object.keys(circle);
+// console.log(keys);
+
+// if ("radius" in circle) console.log("Circle has a radius.");
+
+// Abstraction
+
+// function Circle(radius) {
+//   this.radius = radius;
+
+//   let defaultLocation = { x: 0, y: 0 };
+//   this.getDefaultLocation = function () {
+//     return defaultLocation;
+//   };
+//   this.draw = function () {
+//     console.log("draw");
+//   };
+
+//   Object.defineProperty(this, "defaultLocation", {
+//     get: function () {
+//       return defaultLocation;
+//     },
+//     set: function (value) {
+//       if (!value.x || !value.y) throw new Error("Invalid location");
+//       defaultLocation = value;
+//     },
+//   });
+// }
+
+// const circle = new Circle(10);
+// circle.defaultLocation = 1;
+// console.log(circle.defaultLocation);
+
+// const sw = new Stopwatch()
+
+// Methods in Stopwatch function
+
+// sw.start
+// sw.stop
+// sw.duration
+// sw.reset
