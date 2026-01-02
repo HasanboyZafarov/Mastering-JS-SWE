7c4458038c4bc41526b7f380422a82b86cb9b826
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-02 17:17:20 +0500

    Object Literals

---
+++ Object-oriented Programming in JavaScript/js/objects.js
@@ -0,0 +1,17 @@
+// Object Literals
+
+// const circle = {
+//   radius: 1,
+//   location: { x: 1, y: 1 },
+//   draw: function () {
+//     console.log("draw");
+//   },
+// };
+
+// properties: radius, location
+// methods: function
+
+// properties are used to hold data
+// methods are used to define some logic
+
+// circle.draw()
