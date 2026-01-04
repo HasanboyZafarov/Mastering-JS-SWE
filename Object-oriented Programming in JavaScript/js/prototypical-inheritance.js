83cb72c91f9d4ddf106d2525eda4a67106615629
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 15:33:57 +0500

    Mixins

--- Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
+++ Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
@@ -138,3 +138,45 @@
 // for (let shape of shapes) {
 //   shape.duplicate();
 // }
+
+// Mixins
+// function mixin(target, ...sources) {
+//   Object.assign(target, ...sources);
+// }
+
+// const canEat = {
+//   eat: function () {
+//     this.hunger--;
+//     console.log("eating");
+//   },
+// };
+
+// const canWalk = {
+//   walk: function () {
+//     console.log("walking");
+//   },
+// };
+
+// const canSwim = {
+//   swim: function () {
+//     console.log("swiming");
+//   },
+// };
+
+// const canBark = {
+//   bark: function () {
+//     console.log("barking");
+//   },
+// };
+
+// function Person() {}
+// mixin(Person.prototype, canEat, canWalk);
+// const p = new Person();
+
+// function Goldfish() {}
+// mixin(Goldfish.prototype, canEat, canSwim);
+// const g = new Goldfish();
+
+// function Dog() {}
+// mixin(Dog.prototype, canEat, canBark, canSwim, canWalk);
+// const d = new Dog();
