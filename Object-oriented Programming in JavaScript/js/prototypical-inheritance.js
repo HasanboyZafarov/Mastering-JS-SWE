33207c779dfb9ad2bb2bd14fbeaa6049535ab11e
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 13:11:45 +0500

    Prototypical Inheritance

---
+++ Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
@@ -0,0 +1,24 @@
+// Prototypical inheritance
+
+// function Shape() {}
+
+// Shape.prototype.duplicate = function () {
+//   console.log("duplicate");
+// };
+
+// function Circle(radius) {
+//   this.radius = radius;
+// }
+
+// Circle.prototype = Object.create(Shape.prototype);
+
+// Circle.prototype.draw = function () {
+//   console.log("draw");
+// };
+
+// const s = new Shape();
+// const c = new Circle();
+
+// Whenever we reset the prototype of an object, we need to set the constructor property back to the original constructor function.
+// Circle.prototype.constructor = Circle;
+
