1fc7b78be6380940e8552f76668bd6e5fbc3b95e
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 18:33:26 +0500

    Private properties and methods

--- Object-oriented Programming in JavaScript/js/es6-classes.js
+++ Object-oriented Programming in JavaScript/js/es6-classes.js
@@ -76,3 +76,19 @@
 // const c = new Circle();
 // const draw = c.draw;
 // draw();
+
+// Private Properties and Methods
+// const _radius = Symbol();
+// const _draw = Symbol();
+// class Circle {
+//   constructor(radius) {
+//     // this.radius = radius;
+//     // this["radius"] = radius;
+//     this[_radius] = radius;
+//   }
+//   [_draw]() {}
+// }
+
+// const c = new Circle(1);
+// const key = Object.getOwnPropertySymbols(c)[0];
+// console.log(c[key]);
