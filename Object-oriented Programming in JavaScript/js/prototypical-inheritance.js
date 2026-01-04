3730acd0e37faa7db83eda9395a35dc403b74bfd
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 13:55:27 +0500

    Calling the Super constructor

--- Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
+++ Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
@@ -22,3 +22,26 @@
 // Whenever we reset the prototype of an object, we need to set the constructor property back to the original constructor function.
 // Circle.prototype.constructor = Circle;
 
+// Calling the parent constructor function from the child constructor function
+
+// function Shape(color) {
+//   this.color = color;
+// }
+
+// Shape.prototype.duplicate = function () {
+//   console.log("duplicate");
+// };
+
+// function Circle(radius, color) {
+//   Shape.call(this, color);
+//   this.radius = radius;
+// }
+
+// Circle.prototype = Object.create(Shape.prototype);
+
+// Circle.prototype.draw = function () {
+//   console.log("draw");
+// };
+
+// const s = new Shape();
+// const c = new Circle();
