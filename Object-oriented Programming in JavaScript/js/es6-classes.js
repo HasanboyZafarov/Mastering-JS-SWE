693c595c0d442ead8003078a8d3e88c0855ab072
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 18:01:42 +0500

    Static Methods

--- Object-oriented Programming in JavaScript/js/es6-classes.js
+++ Object-oriented Programming in JavaScript/js/es6-classes.js
@@ -33,3 +33,33 @@
 
 // Class Expression
 // const Square = class {};
+
+// Static Methods
+
+// class Circle {
+//   constructor(radius) {
+//     this.radius = radius;
+//   }
+
+//   // instance method
+//   draw() {
+//     console.log("draw");
+//   }
+
+//   // Static Method
+//   static parse(str) {
+//     const radius = JSON.parse(str).radius;
+//     return new Circle(radius);
+//   }
+// }
+
+// const circle = Circle.parse('{"radius":1}');
+// console.log(circle);
+
+// class Math2 {
+//   static abs(value) {
+//     // ...
+//   }
+// }
+
+// Math.abs(2);
