9e14fa36664becbb58f1aca2c8b7c408a784fed8
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 14:02:54 +0500

    Intermediate function inheritance

--- Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
+++ Object-oriented Programming in JavaScript/js/prototypical-inheritance.js
@@ -1,5 +1,7 @@
 // Prototypical inheritance
 
+const { Children } = require("react");
+
 // function Shape() {}
 
 // Shape.prototype.duplicate = function () {
@@ -45,3 +47,37 @@
 
 // const s = new Shape();
 // const c = new Circle();
+
+// Intermeddiate function inheritance
+
+// function Shape(color) {
+//   this.color = color;
+// }
+
+// Shape.prototype.duplicate = function () {
+//   console.log("duplicate");
+// };
+
+// function extend(Child, Parent) {
+//   Child.prototype = Object.create(Parent.prototype);
+//   Child.prototype.constructor = Child;
+// }
+
+// function Circle(radius, color) {
+//   Shape.call(this, color);
+//   this.radius = radius;
+// }
+
+// extend(Circle, Shape);
+
+// Circle.prototype.draw = function () {
+//   console.log("draw");
+// };
+
+// function Square(size) {
+//   this.size = size;
+// }
+// extend(Square, Shape);
+
+// Square.prototype = Object.create(Shape.prototype);
+// Square.prototype.constructor = Square;
