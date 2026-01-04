6348238a147049f5c31fd5a3012f8ef9d748351f
Hasanboy Zafarov <dewozto1337@gmail.com>
2026-01-04 18:58:58 +0500

    Inheritance

--- Object-oriented Programming in JavaScript/js/es6-classes.js
+++ Object-oriented Programming in JavaScript/js/es6-classes.js
@@ -134,3 +134,25 @@
 
 // const c = new Circle(1);
 
+// Inheritance
+
+// class Shape {
+//   constructor(color) {
+//     this.color = color;
+//   }
+//   move() {
+//     console.log("move");
+//   }
+// }
+
+// class Circle extends Shape {
+//   constructor(color, radius) {
+//     super(color);
+//     this.radius = radius;
+//   }
+//   draw() {
+//     console.log("draw");
+//   }
+// }
+
+// const c = new Circle("red", 1);
